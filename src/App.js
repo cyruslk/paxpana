9ff0b3a845d29ccb396aa0b3de3cc875e9946046
cyrus <cyrus7@hotmail.fr>
2021-07-12 16:11:59 -0400

    Feat: landing page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import Transition from "./Transition";
-import ComingSoon from "./ComingSoon";
-import TextRainingEffect from "./textrainingeffects/TextRainingEffect";
-import WatermarksEffect from "./watermarks/WatermarksEffect";
-import VerticalSlider from "./verticalslider/VerticalSliderEffect";
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
 
-  const renderVideo = () => {
-    return (
-      <video
-      loop={true} 
-      muted={true}
-      autoPlay={true}>
-      <source 
-        src="https://res.cloudinary.com/www-c-t-l-k-com/video/upload/v1623450039/paxpana/Cloud_-_48503_1.mp4" 
-        type="video/mp4"
-      />
-    </video>
-    )
-  }
+  let img = [
+    "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1624302693/paxpana/inspirations/homepage/577204.jpg",
+    "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1624303082/paxpana/inspirations/main/unnamed_5.jpg",
+  ];
 
-  const renderTransition = () => {
-    return (
-      <Transition />
-    )
-  };
+  const [count, setCount] = useState(0);
 
-  const comingSoon = () => {
-    return (
-      <ComingSoon />
-    )
-  }
+  useEffect(() => {
+      if(count === img.length){
+        setCount(0);
+      }else{
+        return null;
+      }
+  }, [count, img.length]);
+
 
   return (
-    <div>
-      {renderTransition()}
-      {comingSoon()}
-      {renderVideo()}
-    </div>
+    <main onClick={() => setCount(count + 1)}>
+        <section className="coming_soon first">
+            <h1>pax pana</h1>
+            <h1>coming soon</h1>
+        </section> 
+        <section className="coming_soon second">
+            <h1>thank you!</h1>
+        </section>
+        <section>
+            <img 
+              alt="img"
+              src={img[count]}
+            />
+        </section>
+    </main>
   );
 }
 
