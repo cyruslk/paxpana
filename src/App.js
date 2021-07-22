7149679b476fb983447bd02071801cee17542bc0
cyrus <cyrus7@hotmail.fr>
2021-07-22 17:31:32 +0200

    Feat: for build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+import { useMailChimp } from 'react-use-mailchimp-signup';
 import {useMediaQuery, useMediaQueries} from '@react-hook/media-query'
 import useSound from 'use-sound';
 import submission from './assets/submission.mp3';
 import click from './assets/click.mp3';
 
 import './App.css';
-
-
-const url = "//xxxx.us13.list-manage.com/subscribe/post?u=zefzefzef&id=fnfgn";
 
 
 let img = [
@@ -18,9 +15,16 @@ let img = [
   "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626123418/paxpana/coming_soon/bath.jpg",
 ];
 
+const url = "https://gmail.us6.list-manage.com/subscribe/post?u=529cf141a9d5c3aec045e67da&amp;id=c2ad5d33b2";
+
 let randomImg = [...img].sort(() => Math.random() - 0.5);
 
 function App() {
+
+  const { error, loading, status, subscribe, message } = useMailChimp({
+    action: url,
+  });
+
 
   const [imgArray, setImgArray] = useState([]);   
   const [count, setCount] = useState(0);
@@ -35,13 +39,13 @@ function App() {
 
 
   useEffect(() => {
-    playClick();
-    if(count === img.length){
+   if(count === img.length){
       setCount(0);
     }else{
       return null;
     }
   }, [count, img.length]);
+
 
   const handleInputChange = (event) => {
     event.persist();
@@ -49,6 +53,16 @@ function App() {
       ...inputs,
       [event.target.name]: event.target.value,
     }));
+  };
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    if (inputs) {
+      playSubmission();
+      subscribe(inputs);
+    }
   };
  
   const handleResetImages = () => {
@@ -62,20 +76,34 @@ function App() {
 
   return (
     <main>
+
         <img 
             id="layer"
             alt="refresh"
             src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626190843/paxpana/coming_soon/ComingSoon.Desktop.jpg" 
         />
+
         <section className="coming_soon first">
             <h1>pax pana</h1>
             <h2>coming soon</h2>
         </section> 
+        
         <section className="coming_soon second">
             <h3>thank you!</h3>
             <div className="mailchimp_input">
-             <MailchimpSubscribe 
-              url={url}/>
+
+             <section>
+
+              <form onSubmit={handleSubmit}>
+                <input type="email" name="email" id="mchimpEmail" onChange={handleInputChange} />
+              </form>
+
+              {error && <p>ERROR</p>}
+              {loading && <p>...Loading</p>}
+              {status && <p>{status}</p>}
+
+            </section>
+
             </div>
         </section>
 
@@ -91,6 +119,7 @@ function App() {
           alt="refresh"
           src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626200996/paxpana/refresh_button_70_opacity.png" 
         />
+
     </main>
   );
 }
