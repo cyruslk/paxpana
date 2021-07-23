3dac9e8f208965f7a8beeec02cfe624da280fb7f
cyrus <cyrus7@hotmail.fr>
2021-07-24 00:05:18 +0200

    Feat: adjustments pt.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import click from './assets/click.mp3';
 
 import './App.css';
 
-
 let img = [
   "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626123426/paxpana/coming_soon/room.jpg",
   "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626123422/paxpana/coming_soon/dog.jpg",
@@ -17,35 +16,52 @@ let img = [
 
 const url = "https://gmail.us6.list-manage.com/subscribe/post?u=529cf141a9d5c3aec045e67da&amp;id=c2ad5d33b2";
 
-let randomImg = [...img].sort(() => Math.random() - 0.5);
+// let randomImg = [...img].sort(() => Math.random() - 0.5);
 
 function App() {
 
-  const { error, loading, status, subscribe, message } = useMailChimp({
-    action: url,
-  });
+  const { error, loading, status, subscribe, message } = useMailChimp(
+    {action: url}
+  );
 
 
   const [imgArray, setImgArray] = useState([]);   
-  const [count, setCount] = useState(0);
+  const [countImg, setCountImg] = useState(0);
+  const [isPlaceholder, setIsPlaceholder] = useState(true);
   const [inputs, setInputs] = useState({});
-
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  
   const [playSubmission] = useSound(submission);
   const [playClick] = useSound(click);
 
+  // useEffect(() => {
+  //   setImgArray(randomImg);
+  // }, []);
+
   useEffect(() => {
-    setImgArray(randomImg);
+    setImgArray(img);
   }, []);
 
 
   useEffect(() => {
-   if(count === img.length){
-      setCount(0);
+   if(countImg === img.length){
+      setCountImg(0);
     }else{
       return null;
     }
-  }, [count, img.length]);
+  }, [countImg, img.length]);
 
+  const returnPlaceholderName = () => {
+    if(isPlaceholder){
+      return "email address";
+    }else{
+      return "";
+    }
+  };
+
+const handlePlaceholderNameChange = () => {
+  setIsPlaceholder(!isPlaceholder);
+};
 
   const handleInputChange = (event) => {
     event.persist();
@@ -62,16 +78,41 @@ function App() {
     if (inputs) {
       playSubmission();
       subscribe(inputs);
+
+      // 
+      if(error){
+        setIsSubscribed("Oops, something went wrong")
+      }
+      if(loading){
+        setIsSubscribed("Loading")
+      }
+      if(!error){
+        setIsSubscribed("thank you")
+      }
+      document.getElementById("emailSubscribeForm").reset();
     }
   };
  
-  const handleResetImages = () => {
-    setImgArray(img);
-    setCount(0);
+  // const handleResetImages = () => {
+  //   setImgArray(img);
+  //   setCountImg(0);
+  // };
+
+  const handlePlayClick = () => {
+    playClick();
+    setCountImg(countImg + 1);
+  };
+
+  const returnSubscriptionMessage = () => {
+    if(isSubscribed){
+      return "thank you!"
+    }else{
+      return null;
+    }
   }
 
   let style = {
-    backgroundImage: `url(${imgArray[count]})`
+    backgroundImage: `url(${imgArray[countImg]})`
   };
 
   return (
@@ -89,32 +130,35 @@ function App() {
         </section> 
         
         <section className="coming_soon second">
-            <h3>thank you!</h3>
+            <h3>{returnSubscriptionMessage()}</h3>
             <div className="mailchimp_input">
-
              <section>
-
-              <form onSubmit={handleSubmit}>
-                <input type="email" name="email" id="mchimpEmail" onChange={handleInputChange} />
+              <form 
+                  id="emailSubscribeForm"
+                  onSubmit={handleSubmit}
+              >
+                <input
+                  onClick={handlePlaceholderNameChange}
+                  onBlur={handlePlaceholderNameChange}
+                  placeholder={returnPlaceholderName()}
+                  type="email" 
+                  name="email" 
+                  id="mchimpEmail" 
+                  onChange={handleInputChange} 
+                />
               </form>
-
-              {error && <p>ERROR</p>}
-              {loading && <p>...Loading</p>}
-              {status && <p>{status}</p>}
-
             </section>
-
             </div>
+
         </section>
 
         <section 
           style={style}
           className="img_section"
-          onClick={() => setCount(count + 1)}
         />
 
         <img 
-          onClick={handleResetImages}
+          onClick={handlePlayClick}
           className="refresh"
           alt="refresh"
           src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626200996/paxpana/refresh_button_70_opacity.png" 
