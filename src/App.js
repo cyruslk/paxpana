6449ad19b95344fc0d83da5031fdc3e07da279f0
cyrus <cyrus7@hotmail.fr>
2021-08-02 19:21:52 +0200

    Refactor: new styling

--- src/App.js
+++ src/App.js
@@ -5,54 +5,32 @@ import submission from './assets/submission.mp3';
 import click from './assets/click.mp3';
 import './App.css';
 
-
 const mailchimpURL = "https://gmail.us6.list-manage.com/subscribe/post?u=529cf141a9d5c3aec045e67da&amp;id=c2ad5d33b2";
 
-let imgDesktop = [
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627128951/paxpana/coming_soon/desktop_img_1.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627128947/paxpana/coming_soon/desktop_img_2.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627128951/paxpana/coming_soon/desktop_img_3.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627128951/paxpana/coming_soon/desktop_img_4.jpg"
-];
-
-let imgMobile = [
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627137179/paxpana/coming_soon/mobile_img_1.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627137180/paxpana/coming_soon/mobile_img_2.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627137179/paxpana/coming_soon/mobile_img_3.jpg",
-  "https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627164713/paxpana/coming_soon/mobile_img_4.jpg"
-]
 
 function App() {
 
-
+  useFavicon("https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1627910449/paxpana/coming_soon/Favicon.ico");
 
   const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
+    width: null,
+    height: null,
   });
 
-  const [imgArray, setImgArray] = useState([]);   
-  const [countImg, setCountImg] = useState(0);
   const [isPlaceholder, setIsPlaceholder] = useState(true);
   const [inputs, setInputs] = useState({});
   const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isShowSubscription, setIsShowSubscription] = useState(false);
   
-  const [playSubmission] = useSound(submission);
   const [playClick] = useSound(click);
-
 
   const { error, loading, status, subscribe, message } = useMailChimp(
     {action: mailchimpURL}
   );
 
-
   useEffect(() => {
 
-    // let lockOrientation = window.screen.lockOrientation || window.screen.mozLockOrientation || window.screen.msLockOrientation || window.screen.orientation.lock;
-
-    // lockOrientation('portrait');
-
-    setImgArray(imgDesktop);
+    document.querySelector('video').defaultPlaybackRate = 0.6;
     function handleResize() {
       setWindowSize({
         width: window.innerWidth,
@@ -67,21 +45,21 @@ function App() {
 
 
   useEffect(() => {
-
     if(windowSize.width < 600) {
-      setImgArray(imgMobile);
+      // setVideo mobile;
     }else{
-      setImgArray(imgDesktop);
+      // setVideo desktop;
     }
+  }, [windowSize]);
 
-    if(countImg === imgArray.length){
-        setCountImg(0);
-      }else{
-        return null;
-      }
-  }, [countImg, imgArray.length, windowSize]);
 
-  
+  useEffect(() => {
+    if(isShowSubscription){      
+      setTimeout(() => {
+        setIsShowSubscription(false)
+      }, 2000)
+    };
+  }, [isShowSubscription])
 
   const returnPlaceholderName = () => {
     if(isPlaceholder){
@@ -104,11 +82,12 @@ const handlePlaceholderNameChange = () => {
   };
 
   const handleSubmit = (event) => {
+    setIsShowSubscription(true)
+    
     if (event) {
       event.preventDefault();
     }
     if (inputs) {
-      playSubmission();
       subscribe(inputs);
       if(error){
         setIsSubscribed("Oops, something went wrong")
@@ -122,85 +101,88 @@ const handlePlaceholderNameChange = () => {
       document.getElementById("emailSubscribeForm").reset();
     }
   };
+
+  const handleSubmitArrow = () => {
+    // alert("fvd")
+  }
  
   const handlePlayClick = () => {
     playClick();
-    setCountImg(countImg + 1);
   };
 
   const returnSubscriptionMessage = () => {
-    if(isSubscribed){
+
+    if(isShowSubscription){
       return "thank you!"
-    }else{
+    }else {
       return null;
-    }
+    };
+
   };
 
-  const renderButtonMobile = () => {
-    return (
-      <button onClick={handleSubmit}>
-        SUBMIT
-      </button>
-    )
-  }
-
-  let style = {
-    backgroundImage: `url(${imgArray[countImg]})`
-  };
 
   return (
     <main>
-        <img 
-            id="layer"
-            alt="refresh"
-            src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626190843/paxpana/coming_soon/ComingSoon.Desktop.jpg" 
-        />
-        <section className="coming_soon first">
-            <h1>pax pana</h1>
-            <h2>coming soon</h2>
-        </section> 
-        
-        <section className="coming_soon second">
-            <h3>{returnSubscriptionMessage()}</h3>
-            <div className="mailchimp_input">
-             <section>
-              <form 
-                id="emailSubscribeForm"
-                onSubmit={handleSubmit}>
-
-                <input
-                  onClick={handlePlaceholderNameChange}
-                  onBlur={handlePlaceholderNameChange}
-                  placeholder={returnPlaceholderName()}
-                  type="email" 
-                  name="email" 
-                  id="mchimpEmail" 
-                  onChange={handleInputChange} 
-                />
-              </form>
-
-              {renderButtonMobile()}
-
-            </section>
-            </div>
-
-        </section>
-
-        <section 
-          style={style}
-          className="img_section"
-        />
-
-        <img 
+        <div 
           onClick={handlePlayClick}
-          className="refresh"
-          alt="refresh"
-          src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/v1626200996/paxpana/refresh_button_70_opacity.png" 
-        />
-
+          className="video-container">
+            <video 
+              autoPlay 
+              muted 
+              loop>
+                <source 
+                  src="https://res.cloudinary.com/www-c-t-l-k-com/video/upload/v1627912642/paxpana/background-video.mp4" 
+                  type="video/mp4"/>
+            </video>
+        </div>
+        <section className="outer_container">
+          <section className="coming_soon inner_container">
+              <h1>pax pana</h1>
+              <h2>coming soon</h2>
+              <h3 id="subscription_message">
+                  {returnSubscriptionMessage()}
+              </h3>
+              <div className="mailchimp_input">
+                <section>
+                  <form 
+                    id="emailSubscribeForm"
+                    onSubmit={handleSubmit}>
+                    <input
+                      onClick={handlePlaceholderNameChange}
+                      onBlur={handlePlaceholderNameChange}
+                      placeholder={returnPlaceholderName()}
+                      type="email" 
+                      name="email" 
+                      id="mchimpEmail" 
+                      onChange={handleInputChange} 
+                    />
+                      <div>
+                      <span 
+                        className="load-more"
+                        onClick={handleSubmit}>
+                        </span>
+                      </div>
+                  </form>
+                </section>
+              </div>
+          </section> 
+        </section>
     </main>
   );
 }
+
+
+const useFavicon = href => {
+  useEffect(() => {
+    const link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement("link");
+    link.type = "image/x-icon";
+    link.rel = "shortcut icon";
+    link.href = href;
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }, [href]);
+};
 
 
 export default App;
